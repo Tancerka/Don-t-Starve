1b401ae7cc3ef2de257156e46b048faa85357c5d
Adam <tonieja@gmail.com>
2024-11-17 17:29:47 +0100

    Git commit

--- DontStarve/app/urls.py
+++ DontStarve/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login/",views.logowanie, name="Logowanie")
+    path("login/",views.logowanie, name="login"),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
