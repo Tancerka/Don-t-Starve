8faf3e151fb4c40fa65b65e551a4505442ba2e20
Adam <tonieja@gmail.com>
2024-11-24 19:47:59 +0100

    git commit

--- DontStarve/app/static/script.js
+++ DontStarve/app/static/script.js
@@ -1,19 +1,19 @@
 // script.js
 
-document.getElementById("login-form").addEventListener("submit", function (event) {
-    event.preventDefault();
+// document.getElementById("login-form").addEventListener("submit", function (event) {
+//     event.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
 
-    // Miejsce na integrację z backendem (Python Django)
-    console.log(`Wprowadzone dane: użytkownik: ${username}, hasło: ${password}`);
+//     // Miejsce na integrację z backendem (Python Django)
+//     console.log(`Wprowadzone dane: użytkownik: ${username}, hasło: ${password}`);
 
-    // Przykładowa walidacja danych (do zastąpienia backendem)
-    if (username === "admin" && password === "1234") {
-        alert("Logowanie pomyślne!");
-        window.location.href = "/dashboard";
-    } else {
-        document.getElementById("error-message").textContent = "Nieprawidłowa nazwa użytkownika lub hasło.";
-    }
-});
+//     // Przykładowa walidacja danych (do zastąpienia backendem)
+//     if (username === "admin" && password === "1234") {
+//         alert("Logowanie pomyślne!");
+//         window.location.href = "/dashboard";
+//     } else {
+//         document.getElementById("error-message").textContent = "Nieprawidłowa nazwa użytkownika lub hasło.";
+//     }
+// });
--- DontStarve/app/views.py
+++ DontStarve/app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .logowanie.form import CustomLoginForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -6,4 +8,14 @@ def home(request):
     return render(request, "home.html")
 
 def logowanie(request):
-    return render(request, "resto/index.html")
+    if request.method == 'POST':
+        form = CustomLoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Przekierowanie po zalogowaniu
+    else:
+        form = CustomLoginForm()
+
+    return render(request, "resto/index.html", {'form': form})
+    
